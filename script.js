bb3f72c1d856973c33dee7c588ab46ee9c45df09
Ddeepika123 <112195696+Ddeepika123@users.noreply.github.com>
2026-08-18 03:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // write js code if required
+const image = document.getElementById("flip");
+
+        image.addEventListener("mouseover", function () {
+            image.src =
+                "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
+        });
+
+        image.addEventListener("mouseout", function () {
+            image.src =
+                "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
+        });
